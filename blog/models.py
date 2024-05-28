371d07c6889267fd3440a4ad501b2238f9d9fe86
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-05-28 19:01:06 +0330

    update post model of blog

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 class Post(models.Model):
+    # image
+    # author
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # tag
+    # category
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_datetime = models.DateTimeField(null=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
+    
+    
