82a33729bf3f3868dc27ba5e3c00df1250781f6c
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-06-03 12:59:52 +0330

    add search feature

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog_list_view, name='list_view'),
-    path('<int:pk>', views.blog_detail_view, name='detail_view'),
-    path('category/<str:cat_name>', views.blog_list_view, name='category'),
+    path('category/<str:cat_name>/', views.blog_list_view, name='category'),
+    path('search/', views.blog_search_view, name='search'),
+    path('<int:pk>/', views.blog_detail_view, name='detail_view'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,32 +10,32 @@ def blog_list_view(request, **kwargs):
     posts = Post.objects.filter(status=True)
     posts = posts.filter(published_datetime__lte=now())
 
-    if kwargs.get('cat_name') != None :
-        posts = posts.filter(category__name=kwargs.get('cat_name'))
-    
+    if kwargs.get("cat_name") != None:
+        posts = posts.filter(category__name=kwargs.get("cat_name"))
+
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
 
 def blog_detail_view(request, pk):
     queryset = Post.objects.filter(status=True, published_datetime__lte=now())
-    
+
     post = get_object_or_404(queryset, id=pk)
     post.counted_view += 1
     post.save()
-    
-    next_post = queryset.filter(id__gt=post.id).order_by('id').first()   
-    previous_post = queryset.filter(id__lt=post.id).order_by("id").last()   
-    
-       
-         
+
+    next_post = queryset.filter(id__gt=post.id).order_by("id").first()
+    previous_post = queryset.filter(id__lt=post.id).order_by("id").last()
 
     context = {"post": post, "next_post": next_post, "previous_post": previous_post}
     return render(request, "blog/blog-single.html", context)
 
 
-def blog_category(request, cat_name):
+def blog_search_view(request):
     posts = Post.objects.filter(status=True, published_datetime__lte=now())
-    posts = posts.filter(category__name=cat_name)
+    if request.method == "GET":
+        if s := request.GET.get("q"):
+            posts = posts.filter(content__contains=s)
+
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
